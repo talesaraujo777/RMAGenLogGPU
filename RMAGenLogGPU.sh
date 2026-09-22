3a1d6cab183612a29e4ceafcc2a502c9b01760e0
Tales Araujo <158984674+talesaraujo777@users.noreply.github.com>
2026-09-22 07:19:34 -0400

    Fix ERROS variable initialization in RMAGenLogGPU.sh
    Reinitialize the ERROS variable to 0 after installation check.

--- RMAGenLogGPU.sh
+++ RMAGenLogGPU.sh
@@ -2,8 +2,7 @@
 
 set -u
 
-ERROS=0
-
+# Cores para textos
 RED='\033[0;31m'
 GREEN='\033[0;32m'
 YELLOW='\033[1;33m'
@@ -28,6 +27,8 @@ EOF
 # ------------------------------------------------------------------------
 # Instala o IPMITool
 # ------------------------------------------------------------------------
+
+ERROS=0
 
 if sudo apt-get install -y ipmitool >/dev/null 2>&1; then
     echo -e "Executando RMAGenLogGPU..."
